0aa3ba553783ac2dc347a15f07cc595132d5fc04
Daniel Larmour <daniellarmour@hotmail.com>
2017-07-30 12:25:09 +1200

    Introduction of Appbar Cannot use AppCompat so the use of AppCompat Delegat has to be used

--- MyApplication/app/src/main/java/larmour/daniel/theaccountingapp/ui/MainActivity.java
+++ MyApplication/app/src/main/java/larmour/daniel/theaccountingapp/ui/MainActivity.java
@@ -1,6 +1,7 @@
 package larmour.daniel.theaccountingapp.ui;
 
 import android.arch.lifecycle.LifecycleActivity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -9,6 +10,7 @@ import android.support.v7.app.AppCompatCallback;
 import android.support.v7.app.AppCompatDelegate;
 import android.support.v7.view.ActionMode;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
 
 import io.reactivex.disposables.CompositeDisposable;
@@ -17,17 +19,18 @@ import larmour.daniel.theaccountingapp.R;
 /**
  * Main screen of the app
  */
-public class MainActivity extends LifecycleActivity implements AppCompatCallback{
+public class MainActivity extends LifecycleActivity implements AppCompatCallback {
     private static final String TAG = MainActivity.class.getSimpleName();
 
     private       ViewModelFactory    mViewModelFactory;
     private       UserViewModel       mViewModel;
     private final CompositeDisposable mDisposable = new CompositeDisposable();
     private       AppCompatDelegate   mDelegate;
-    private       BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
                     return true;
@@ -43,19 +46,42 @@ public class MainActivity extends LifecycleActivity implements AppCompatCallback
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setupAppBar(savedInstanceState);
 
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+    private void setupAppBar(Bundle savedInstanceState) {
         // Because we can't use AppCompatActivity
         mDelegate = AppCompatDelegate.create(this, this);
         mDelegate.onCreate(savedInstanceState);
         mDelegate.setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.layout_toolbar);
+        toolbar.inflateMenu(R.menu.settings);
         mDelegate.setSupportActionBar(toolbar);
         if(mDelegate.getSupportActionBar() != null) {
             mDelegate.getSupportActionBar().setTitle(TAG);
         }
+    }
 
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        if(mDelegate.getSupportActionBar() != null) {
+            getMenuInflater().inflate(R.menu.settings, menu);
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onMenuItemSelected(int featureId, MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_profile:
+                Intent intent = new Intent(MainActivity.this, ProfileActivity.class);
+                startActivity(intent);
+                break;
+        }
+        return true;
     }
 
     @Override
